3d65dde6bdb1a6dff27a8521e10043b347a3d8e4
MelMino <160610954+MelMino@users.noreply.github.com>
2024-06-24 12:26:10 -0400

    Add files via upload

---
+++ hello-dojo-app/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Lista from './Lista'
+
+function App() {
+  return (
+    <>
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <Lista />
+    </>
+
+  )
+}
+
+export default App
---
+++ hello-dojo-app/src/Lista.jsx
@@ -0,0 +1,20 @@
+/*@author Mel Miño*/
+/**
+ * Representa una lista de tareas 
+ * @returns elemento  JSX que contiene una lista no ordenada
+ */
+const Lista = () => {
+    return (
+        <>
+            <ul>
+                <li>Learn React</li>
+                <li>Climb Mt. Everest</li>
+                <li>Run a marathon</li>
+                <li>Feed the dogs</li>
+            </ul>
+
+        </>
+    )
+}
+export default Lista; 
+//Permite que la clae lista pueda ser exportada a otros archivos del proyecto
